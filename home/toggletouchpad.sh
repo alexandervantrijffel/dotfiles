bd62255d48d1be5dfe7b4d30a75be48697f2e613
Alexander <lexmail@gmail.com>
2018-09-08 16:16:45 +0200

    Disable/enable Lenovo touchpad

--- home/toggletouchpad.sh
+++ home/toggletouchpad.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 declare -i ID
-ID=`xinput list | grep -Eo 'Generic Mouse\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
-echo id is $ID 
+
+ID=`xinput list | grep -Eo 'Synaptics TM3288-003\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}' | tail -n1`
+if [ -z "$ID" ]; then
+  echo "No Synaptics TM3288-003 touchpad found."
+  return
+fi
+
+# ID=`xinput list | grep -Eo 'Generic Mouse\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
 declare -i STATE
 STATE=`xinput list-props $ID|grep 'Device Enabled'|awk '{print $4}'`
 if [ $STATE -eq 1 ]
