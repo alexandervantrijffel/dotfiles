689d4d0a993115391f3d7e3e8258ecb8be80754b
Alexander <lexmail@gmail.com>
2019-02-11 22:54:41 +0100

    install commands

--- install/newmachine.sh
+++ install/newmachine.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
-# no last login message on mac
-touch ~/.hushlogin
-
 THISDIR=$(cd `dirname $0` && pwd)
 DOTFILES="$(dirname "$THISDIR")"
 
 if [ "$(uname)" == "Darwin" ]; then
+  # no last login message on mac
+  touch ~/.hushlogin
+
   brew install ack fortune cowsay lolcat
 else
-  apt update && apt install the-silver-searcher
+  sudo apt update && sudo apt install curl zsh the-silver-searcher feh htop fortune shutter -y
+  
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf 
+  ~/.fzf/install 
+
+  sudo dpkg -i bat_0.10.0_amd64.deb # adapt version number and architecture
+
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+  # sudo apt-get build-dep shotwell
+  # sudo apt-get install yelp-tools appstream-util libgcr-3-dev libgdata-dev libwebp-dev
 fi
 
 FORTUNES="/usr/share/games/fortunes"
