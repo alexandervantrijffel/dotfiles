4b1423f33fbde6274b238a71e293b86725020ba6
Alexander <lexmail@gmail.com>
2020-08-17 22:09:22 +0200

    qute

--- home/qutebrowser/.config/qutebrowser/config.py
+++ home/qutebrowser/.config/qutebrowser/config.py
@@ -539,12 +539,12 @@
 ##   - no-3rdparty: Accept cookies from the same origin only. This is known to break some sites, such as GMail.
 ##   - no-unknown-3rdparty: Accept cookies from the same origin only, unless a cookie is already set for the domain. On QtWebEngine, this is the same as no-3rdparty.
 ##   - never: Don't accept cookies at all.
-# c.content.cookies.accept = 'all'
+c.content.cookies.accept = 'all'
 
 ## Store cookies. Note this option needs a restart with QtWebEngine on Qt
 ## < 5.9.
 ## Type: Bool
-# c.content.cookies.store = True
+c.content.cookies.store = True
 
 ## Default encoding to use for websites. The encoding must be a string
 ## describing an encoding such as _utf-8_, _iso-8859-1_, etc.
--- home/qutebrowser/qutebrowser.sh
+++ home/qutebrowser/qutebrowser.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/data/dev/github.com/qutebrowser/.venv/bin/python3 -m qutebrowser $@
+/data/dev/github.com/qutebrowser/.venv/bin/python3 -m qutebrowser "$@"
