567b88fef54877b9d177389de0338af609271b90
Alexander van Trijffel <lexmail@gmail.com>
2021-02-25 19:27:16 +0100

    fix: CTRL+R and CTRL+T with fzf

---
+++ home/fzf/postinit.sh
@@ -0,0 +1,4 @@
+# unfortunately we have to repeat this after zsh-vim-mode has been loaded
+source $ZPLUG_REPOS/junegunn/fzf/shell/key-bindings.zsh
+source $ZPLUG_REPOS/junegunn/fzf/shell/completion.zsh
+
