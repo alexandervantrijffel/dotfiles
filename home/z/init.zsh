adb5e69835bd2017451f80c2edf5e461abdbca0b
Alexander van Trijffel <lexmail@gmail.com>
2021-04-13 10:55:46 +0200

    feat: search-service mappings

--- home/z/init.zsh
+++ home/z/init.zsh
@@ -7,3 +7,7 @@ alias zfbew="z ps-737migration-be && cd workspace"
 alias zffe="z ps-licenseadmin-fe"
 alias zffev="z ps-licenseadmin-fe && v"
 alias zffey="z ps-licenseadmin-fe && yrs"
+
+alias zss="z search-service"
+alias zssv="z search-service && v"
+alias zssy="z search-service && yarn serve"
