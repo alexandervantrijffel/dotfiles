f733684169f7cbc067a7325a2c9c60e1a49fffd9
Alexander van Trijffel <lexmail@gmail.com>
2021-04-30 11:59:29 +0200

    feat: ignore test output for fzf

--- fzf/init.zsh
+++ fzf/init.zsh
@@ -6,11 +6,11 @@ zplug "junegunn/fzf-bin",\
         source $ZPLUG_REPOS/junegunn/fzf/shell/key-bindings.zsh
         source $ZPLUG_REPOS/junegunn/fzf/shell/completion.zsh
         """
-        export FZF_DEFAULT_COMMAND='fd --hidden --type file --no-ignore --exclude "/.git/" --exclude "/dist"'
+        #export FZF_DEFAULT_COMMAND='fd --hidden --type file --no-ignore --exclude "/.git/" --exclude "/dist" --exclude "/test-reports" --exclude "/coverage"'
         # export FZF_DEFAULT_OPTS='--ansi --height 40% --reverse --no-border --multi'
         export FZF_DEFAULT_OPTS='--ansi --multi --height 80%'
         # export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-        export FZF_DEFAULT_COMMAND='fd --type file --hidden --follow --no-ignore --exclude .git --exclude node_modules --exclude dist'
+        export FZF_DEFAULT_COMMAND='fd --type file --hidden --follow --no-ignore --exclude .git --exclude node_modules --exclude dist --exclude coverage --exclude test-reports'
         export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 zplug "junegunn/fzf",\
     as:command,\
