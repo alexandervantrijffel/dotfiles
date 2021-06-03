e27b3e35ce74adfd2525575ea3d26a27e16b4c01
Alexander van Trijffel <lexmail@gmail.com>
2021-06-03 18:19:00 +0200

    feat: dcdv

---
+++ docker/env.zsh
@@ -0,0 +1,8 @@
+if type docker 1>/dev/null; then
+  alias d="docker "
+fi
+if type docker-compose 1>/dev/null; then
+  alias dc="docker-compose "
+  alias dcdv="docker-compose down -v"
+fi
+
--- docker/init.zsh
+++ docker/init.zsh
@@ -1,9 +1,3 @@
 zplug "plugins/docker", from:oh-my-zsh, defer:3
 zplug "plugins/docker-compose", from:oh-my-zsh, defer:3
 
-if type docker 1>/dev/null; then
-  alias d="docker "
-fi
-if type docker-compose 1>/dev/null; then
-  alias dc="docker-compose "
-fi
