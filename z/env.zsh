a175d8a0c48e280059ba6429fd1488966d07f92f
Alexander van Trijffel <lexmail@gmail.com>
2021-06-03 16:54:20 +0200

    feat: zdsc

---
+++ z/env.zsh
@@ -0,0 +1,16 @@
+alias zdsc="z dev-service-composition"
+
+alias zfbe="z ps-737migration-be"
+alias zfbev="z ps-737migration-be && v"
+alias zfbew="z ps-737migration-be/workspace"
+
+alias zffe="z ps-licenseadmin-fe"
+alias zffev="z ps-licenseadmin-fe && v"
+alias zffey="z ps-licenseadmin-fe && yrs"
+
+alias zg="z grandvisionhq"
+
+alias zss="z search-service"
+alias zssv="z search-service && v"
+alias zssy="z search-service && yarn serve"
+
--- z/init.zsh
+++ z/init.zsh
@@ -1,15 +1,2 @@
 zplug "rupa/z", use:"*.sh", hook-build:"touch ${HOME}/.z", defer:2
 
-alias zfbe="z ps-737migration-be"
-alias zfbev="z ps-737migration-be && v"
-alias zfbew="z ps-737migration-be/workspace"
-
-alias zffe="z ps-licenseadmin-fe"
-alias zffev="z ps-licenseadmin-fe && v"
-alias zffey="z ps-licenseadmin-fe && yrs"
-
-alias zg="z grandvisionhq"
-
-alias zss="z search-service"
-alias zssv="z search-service && v"
-alias zssy="z search-service && yarn serve"
