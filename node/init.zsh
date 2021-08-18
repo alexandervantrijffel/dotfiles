f5d8b62b3c340a72e71915448551078a68cb26f3
Alexander van Trijffel <lexmail@gmail.com>
2021-08-18 14:59:40 +0200

    feat: nvm config

--- node/init.zsh
+++ node/init.zsh
@@ -1,2 +1,6 @@
 # NPM_TOKEN is set in ~/.zshrc.local
 # export NPM_TOKEN=$(cat ~/.npmrc | sed 's/.*authToken=\(.*\)/\1/')
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
