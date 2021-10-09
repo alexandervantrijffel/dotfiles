d64833e6e2752133e5f038acab71df60e76167cd
Alexander van Trijffel <lexmail@gmail.com>
2021-10-09 17:04:35 +0200

    chore: cleanup

--- z/env.zsh
+++ z/env.zsh
@@ -1,5 +1,3 @@
-alias zdsc="z dev-service-composition"
-
 alias zfbe="z ps-737migration-be"
 alias zfbev="z ps-737migration-be && v"
 alias zfbew="z ps-737migration-be/workspace"
@@ -8,12 +6,5 @@ alias zffe="z ps-licenseadmin-fe"
 alias zffev="z ps-licenseadmin-fe && v"
 alias zffey="z ps-licenseadmin-fe && yrs"
 
-alias zgv="z grandvisionhq"
 alias zga="z alexandervantrijffel"
 alias zgh="z dev/github.com"
-
-alias zss="z search-service"
-alias zssv="z search-service && v"
-alias zssy="z search-service && yarn serve"
-alias zsb="z service-base"
-
