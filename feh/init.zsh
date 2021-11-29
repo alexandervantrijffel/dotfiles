fd4d4fce0cf02d3731984781b1670bff6b76279b
Alexander van Trijffel <lexmail@gmail.com>
2021-11-29 12:13:58 +0100

    fix: fehfg

--- feh/init.zsh
+++ feh/init.zsh
@@ -1,7 +1,5 @@
 if type feh 1>/dev/null; then
-  feh --slideshow-delay 1800 --bg-tile $HOME/Pictures/backgrounds -z -r -D1800
-  # // --bg-fill or --bg-scale?
-
+  # --bg-fill or --bg-scale?
   # or use --zoom fill
-  alias fehfg='nohup feh -zrs --scale-down --image-bg black --slideshow-delay 3600 ~/Pictures/bgsrcpics &'
+  alias fehfg='nohup feh -zrs --scale-down --image-bg black --slideshow-delay 3600 ~/Pictures/fehbackgrounds &'
 fi
--- sh/fehfg.sh
+++
@@ -1,3 +0,0 @@
-feh -zrs --scale-down --image-bg black --slideshow-delay 3600 ~/Pictures/backgrounds
-# or --zoom max
-# or --zoom fill
