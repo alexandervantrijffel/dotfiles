cc5e4365d179a5087e5e2890df67d6f00b68ca65
Alexander van Trijffel <lexmail@gmail.com>
2021-12-26 07:06:37 +0100

    feat: re-add feh background

--- feh/init.zsh
+++ feh/init.zsh
@@ -1,5 +1,9 @@
 if type feh 1>/dev/null; then
+  # background for i3
+  feh --slideshow-delay 1800 --bg-tile $HOME/Pictures/backgrounds -z -r -D1800
   # --bg-fill or --bg-scale?
   # or use --zoom fill
+  
+  # foreground window
   alias fehfg='nohup feh -zrs --scale-down --image-bg black --slideshow-delay 3600 ~/Pictures/fehbackgrounds &'
 fi
