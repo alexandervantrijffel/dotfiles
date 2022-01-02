deed3985784773f604dfafccfb96520aaf591cda
Alexander van Trijffel <lexmail@gmail.com>
2022-01-02 17:18:46 +0100

    feat: rgpu ripgrep with preview for ignored files

--- ripgrep/init.zsh
+++ ripgrep/init.zsh
@@ -1,37 +1,52 @@
 #!/bin/zsh
 zplug 'BurntSushi/ripgrep', from:gh-r, as:command, rename-to:"rg"
 
+# ripgrep with preview
 rgp() {
   outfile=$(mktemp)
   script --flush --quiet --return $outfile --command "rg \"$@\""
+  rgp_exec $outfile "$@"
+}
 
-  local count=$(cat $outfile | wc -l)
+# ripgrep with preview, include files from .gitignore and .ignore
+rgpu() {
+  outfile=$(mktemp)
+  rgoptions='-uu'
+  
+  script --flush --quiet --return $outfile --command "rg $rgoptions \"$@\""
+  rgp_exec $outfile $rgoptions "$@"
+}
+
+rgp_exec() {
+  local count=$(cat $1 | wc -l)
   
   if (( $count == 0 )); then 
     echo Nothing found
   elif (( $count > 4)); then 
-    rg_previewonenter "$@"
+    rg_previewonenter $2 "$3"
   fi
 
   rm ${outfile}
 }
 
 rg_previewonenter() {
+  echo 1 is ${1[@]} , 2 is "$2"
  out=$(rg \
 	--column \
 	--line-number \
 	--no-column \
 	--no-heading \
 	--fixed-strings \
-	--ignore-case \
-	--hidden \
-	--follow \
-    "$@" \
+  $1 \
+  "$@" \
 	| awk -F  ":" '/1/ {start = $2<5 ? 0 : $2 - 5; end = $2 + 5; print $1 " " $2 " " start ":" end}' \
-	| fzf --preview 'bat --wrap character --color always {1} --highlight-line {2} --line-range {3}' --preview-window wrap)
+	| fzf --preview 'bat --wrap character --color always {1} --highlight-line {2} --line-range {3}' \
+    --preview-window wrap)
         
-    read -r filename line <<< "${out}"
+  read -r filename line <<< "${out}"
+  if [[ ! -z $filename ]]; then
     ${EDITOR:-vim} "${filename}" +"normal! ${line}zz"
+  fi
 }
 
 # with ctrl+o command to navigate to line:column number
@@ -40,9 +55,7 @@ rg_previewonctrlo() {
     --column \
     --no-heading \
     --fixed-strings \
-    --ignore-case \
     --hidden \
-    --follow \
      "$@" \
     | awk -F  ":" '/1/ {start = $2<5 ? 0 : $2 - 5; end = $2 + 5; print $1 " " $2 " " $3 " " start ":" end}' \
     | fzf \
