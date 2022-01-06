e44696bfbce1b6128e6d3935a570c525227d0d6d
Alexander van Trijffel <lexmail@gmail.com>
2022-01-06 21:45:39 +0100

    refactor: make fzf alt c command cleaner

--- fzf/init.zsh
+++ fzf/init.zsh
@@ -8,10 +8,11 @@ zplug "junegunn/fzf-bin",\
         """
         export FZF_DEFAULT_OPTS="--bind 'ctrl-l:up,ctrl-k:down,ctrl-a:select-all,ctrl-d:preview-page-down,ctrl-u:preview-page-up' --cycle --ansi --height 80%"
         
+        MY_DEFAULT_COMMAND_OPTS='--hidden --follow --no-ignore --exclude .git --exclude node_modules --exclude dist --exclude coverage --exclude test-reports --exclude .build --exclude build --exclude .serverless --exclude .venv --exclude .pytest_cache --exclude __pycache__ --exclude pygreet.egg-info --exclude .nox --exclude .cache'
         # used by ctrl+p in vim and when running fzf without arguments
-        export FZF_DEFAULT_COMMAND='fd --type file --hidden --follow --no-ignore --exclude .git --exclude node_modules --exclude dist --exclude coverage --exclude test-reports --exclude .build --exclude build --exclude .serverless --exclude .venv --exclude .pytest_cache --exclude __pycache__ --exclude pygreet.egg-info --exclude .nox --exclude .cache'
+        export FZF_DEFAULT_COMMAND="fd --type file $MY_DEFAULT_COMMAND_OPTS"
         
-        export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+        export FZF_CTRL_T_COMMAND="fd __type directory $MY_DEFAULT_COMMAND_OPTS"
         export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND --type directory"
 
 # make sure python is available. Check `zplug --log` in case of errors
