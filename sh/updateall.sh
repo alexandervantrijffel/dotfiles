e513a501e4e66ecad7cd2f05d87ee7c4977045fb
Alexander van Trijffel <lexmail@gmail.com>
2022-05-30 12:57:20 +0200

    chore: clean caches

--- sh/updateall.sh
+++ sh/updateall.sh
@@ -10,11 +10,15 @@ zplug clear
 echo updating yarn packages
 yarn global upgrade
 yarn set version latest
+yarn cache clean --all
+
+npm cache clean --force
 
 echo updating python packages
 sudo python3 -m pip install --upgrade pip
 sudo -H pip install --upgrade pip setuptools wheel
 sudo -H pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo -H python3 -m pip install -U
+sudo -H pip cache purge
 
 echo apt update and upgrade
 sudo apt update
