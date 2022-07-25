23f827a7d14aaa7e7dbb1dfaacc6c357a01e8804
Alexander van Trijffel <lexmail@gmail.com>
2022-07-25 12:28:53 +0200

    feat: remove orphans

--- docker/env.zsh
+++ docker/env.zsh
@@ -8,7 +8,7 @@ if type docker 1>/dev/null; then
 fi
 if type docker-compose 1>/dev/null; then
   alias dc="docker-compose "
-  alias dcdv="docker-compose down -v"
+  alias dcdv="docker-compose down --remove-orphans -v"
   alias dcs="docker-compose stop"
   alias dcrm="docker-compose rm -s -v"
 fi
