1531495ad492573a88da3ff692ba307f2264971e
Alexander van Trijffel <lexmail@gmail.com>
2022-08-11 16:31:37 +0200

    feat: add yarn to path

--- node/init.zsh
+++ node/init.zsh
@@ -4,3 +4,6 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export PATH="$PATH:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin"
+
