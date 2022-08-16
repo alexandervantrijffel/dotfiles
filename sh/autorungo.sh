aaae68649257280a7a1861cc3a60fdca0dc76067
Alexander van Trijffel <lexmail@gmail.com>
2022-08-16 11:00:22 +0200

    chore: agozap wip

--- sh/autorungo.sh
+++ sh/autorungo.sh
@@ -1,2 +1,9 @@
-#!/bin/sh -
-reflex -r '\.go$' -s -- sh -c 'go run -tags=debug  . 2>&1 | zap-pretty'
+#!/bin/sh
+TORUN=${1:-.}
+reflex -r '\.go$' -s -- sh -c 'go run -tags=debug . 2>&1'
+
+# agozap() {
+#   TORUN=${1:-.}
+#   echo Running $TORUN}
+#   reflex -r '\.go$' -s -- sh -c "go run -tags=debug ${TORUN} 2>&1 | zap-pretty"
+# }
