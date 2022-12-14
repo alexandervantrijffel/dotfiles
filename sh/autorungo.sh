125006f25af80d197417a4a15ae10892ff25f1a0
Alexander van Trijffel <lexmail@gmail.com>
2022-12-15 00:50:44 +0100

    feat: support custom ago command

--- sh/autorungo.sh
+++ sh/autorungo.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
-TORUN=${1:-.}
+TORUN=${1:-go run -tags=debug .}
+reflex -r '\.go$' -s -- sh -c "$TORUN 2>&1"
 
-reflex -r '\.go$' -s -- sh -c 'go run -tags=debug cmd/position/main.go 2>&1'
-
-# reflex -r '\.go$' -s -- sh -c 'go run -tags=debug . 2>&1 | zap-pretty'
-# reflex -r '\.go$' -s -- sh -c 'go run -tags=debug . 2>&1'
-
-# agozap() {
-#   TORUN=${1:-.}
-#   echo Running $TORUN}
-#   reflex -r '\.go$' -s -- sh -c "go run -tags=debug ${TORUN} 2>&1 | zap-pretty"
-# }
+# reflex -r '\.go$' -s -- sh -c "$TORUN 2>&1 | zap-pretty"
