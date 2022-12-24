75e91d018b71ac1e2b44124923b83b996ff4a2c5
Alexander van Trijffel <alexander@structura.ws>
2022-12-24 14:13:07 +0000

    fix: fd

--- fd/init.zsh
+++ fd/init.zsh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 if ! type fd 1>/dev/null; then
-    if type fdfind 1>/dev/null; then alias fd="fdfind"; fi
+    if type fdfind 1>/dev/null; then sudo _ ln -s $(which fdfind) /usr/bin/fd; fi
 fi
