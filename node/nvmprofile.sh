d1300376d4bede547afab6f295dbc4864608ca40
Alexander van Trijffel <alexander@structura.ws>
2022-12-25 10:58:47 +0000

    chore: var:

---
+++ node/nvmprofile.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# add simlink to this file to /etc/profile.d/
+
+export NVM_DIR="/usr/local/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
