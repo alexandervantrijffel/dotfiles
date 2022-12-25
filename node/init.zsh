7e34f60751a505ecb30f78b891eac0dce2bf1be0
Alexander van Trijffel <alexander@structura.ws>
2022-12-25 11:49:13 +0000

    chore: set yarn global cache folder

--- node/init.zsh
+++ node/init.zsh
@@ -1,3 +1,6 @@
+#!/bin/zsh
+THISDIR=${0:a:h}
+
 # NPM_TOKEN is set in ~/.zshrc.local
 # export NPM_TOKEN=$(cat ~/.npmrc | sed 's/.*authToken=\(.*\)/\1/')
 
@@ -5,5 +8,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-export PATH="$PATH:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin"
+[ ! -f $HOME/.yarnrc ] && ln -s $THISDIR/.yarnrc.yml $HOME/.yarnrc
 
+export YARN_CACHE_FOLDER=/usr/local/share/.config/yarn/global
