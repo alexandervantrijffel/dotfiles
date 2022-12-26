c5c81bcc4bd69b3181e48148b5ba116c8780de67
Alexander van Trijffel <alexander@structura.ws>
2022-12-26 09:28:44 +0000

    feat: remap  plus minus / section key to grave / tilde

--- keyboard/init.zsh
+++ keyboard/init.zsh
@@ -1,3 +1,6 @@
+#!/bin/zsh
+THISDIR=${0:a:h}
+
 # caps lock = control key
 # re-run this when plugging in another keyboard
 type setxkbmap > /dev/null 2>&1 && setxkbmap -option ctrl:nocaps
@@ -11,3 +14,6 @@ bindkey "^F" forward-word
 
 stty start undef  # disable C-s stopping receiving keyboard signals.
 stty stop undef
+
+[ ! -e ~/.xModmap ] && ln -s $THISDIR/.Xmodmap ~/.xModmap
+xmodmap ~/.xModmap
