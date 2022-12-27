f2654c76d47db48e91b059b2822fbb2ee2a75415
Alexander van Trijffel <lexmail@gmail.com>
2022-12-27 09:38:07 +0100

    fix: setup fd

--- fd/init.zsh
+++ fd/init.zsh
@@ -2,7 +2,6 @@
 if ! type fd 1>/dev/null; then
     if ! type fdfind; then sudo apt install fd-find; fi
 
-    if ! type fdfind 1>/dev/null; then
-      sudo ln -s $(which fdfind) /usr/bin/fd; fi
+    ! type fd 1>/dev/null &&  sudo ln -s $(which fdfind) /usr/bin/fd
     else
 fi
