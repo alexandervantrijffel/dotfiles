d83ecf280711ab94824e2a1c2206e4348e71f978
Alexander van Trijffel <lexmail@gmail.com>
2022-12-27 09:53:10 +0100

    docs: some vmware comments

--- vmware/init.zsh
+++ vmware/init.zsh
@@ -1,4 +1,4 @@
- # for clipboard sharing with host
- if type vmware-user 1>/dev/null; then
-   vmware-user --no-startup-d &> /dev/null 
- fi
+ # for clipboard sharing with host and screen resizing
+type vmware-user 1>/dev/null && vmware-user --no-startup-d &> /dev/null 
+
+# type vmtoolsd 1>/dev/null && vmtoolsd -n vmusr &> /dev/null
