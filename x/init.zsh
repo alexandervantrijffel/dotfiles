afb7e3737b0f2797c3dad62daf589ebc8ab08b66
Alexander van Trijffel <alexander@structura.ws>
2023-03-05 18:40:28 +0000

    chore: don't pollute the parent scope with local variables

--- x/init.zsh
+++ x/init.zsh
@@ -1,2 +1,25 @@
 #!/bin/zsh
 source ${0:a:h}/togglexdpi.zsh
+
+func () {
+  local PREFFILE=$HOME/.Xresources-dpi-preference
+  if [ -s $PREFFILE ]; then
+    local CURPREF=$(cat $PREFFILE)
+  else
+    local CURPREF=lodpi
+  fi
+
+  if [[ $CURPREF =~ "hidpi" ]]; then
+    echo "Activating hidpi"
+    GDK_SCALE=2
+    GDK_DPI_SCALE=0.5
+    QT_AUTO_SCREEN_SCALE_FACTOR=1
+  else
+    echo "Activating lodpi"
+    GDK_SCALE=1
+    GDK_DPI_SCALE=1.0
+    QT_AUTO_SCREEN_SCALE_FACTOR=1
+  fi
+}
+func
+unset -f func
