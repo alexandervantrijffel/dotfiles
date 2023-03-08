6dd2f942cd5c56b3467dc0063515cb470635b47e
Alexander van Trijffel <lexmail@gmail.com>
2023-03-08 11:35:16 +0100

    fix: do enable docker compose zplug for docker aliasses

--- docker/init.zsh
+++ docker/init.zsh
@@ -1,5 +1,5 @@
 zplug "plugins/docker", from:oh-my-zsh, defer:3
-# zplug "plugins/docker-compose", from:oh-my-zsh, defer:3
+zplug "plugins/docker-compose", from:oh-my-zsh, defer:3
 
 if type docker-compose 1>/dev/null; then
   alias dc="docker-compose "
