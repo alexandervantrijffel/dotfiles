ae3e55151e20d750b9527c2593125a45d868f766
Alexander van Trijffel <alexander@structura.ws>
2023-03-08 16:27:12 +0000

    chore: move z aliases to interactive shells

--- z/init.zsh
+++ z/init.zsh
@@ -1,2 +1,18 @@
 zplug "rupa/z", use:"*.sh", hook-build:"touch ${HOME}/.z", defer:2
 
+alias zfbe="z ps-737migration-be"
+alias zfbev="z ps-737migration-be && v"
+alias zfbew="z ps-737migration-be/workspace"
+
+alias zffe="z ps-licenseadmin-fe"
+alias zffev="z ps-licenseadmin-fe && v"
+alias zffey="z ps-licenseadmin-fe && yrs"
+
+alias zgha="z alexandervantrijffel"
+alias zgh="z dev/github.com"
+alias zgl="z dev/gitlab.com"
+
+alias ztsb="z trainservicebus"
+alias tsbv="z trainservicebus && v"
+
+alias iosv="z ios && v"
