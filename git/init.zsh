41ed92d81e9afc522a9bd5ac6e815667e2a75ff3
Alexander van Trijffel <lexmail@gmail.com>
2023-03-09 17:36:25 +0100

    feat: gco

--- git/init.zsh
+++ git/init.zsh
@@ -18,6 +18,7 @@ gitalias () {
   alias gcop="gco -p"
   alias gcp="git cherry-pick"
   alias gcpc="git cherry-pick --continue"
+  alias gco="git checkout"
   alias gcm="git commit -S -v -m"
   alias gclean="git checkout -- . && git clean -fd ."
   alias gd="git diff --word-diff -w"
