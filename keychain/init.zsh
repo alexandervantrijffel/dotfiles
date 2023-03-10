9fde569b3f5471dc4ea432d0309fde9447bb154d
Alexander van Trijffel <alexander@structura.ws>
2023-03-10 12:50:14 +0100

    feat: keychain in interactive shell, run startx when needed

--- keychain/init.zsh
+++ keychain/init.zsh
@@ -1,4 +1,4 @@
 if [[ -x $(command -v keychain) ]]; then
-  eval `keychain --agents gpg 6DA187A7C48AE7C7`
-  eval `keychain --eval id_rsa --agents ssh`
+  eval `keychain --agents gpg 6DA187A7C48AE7C7 &> /dev/null`
+  eval `keychain --eval id_rsa --agents ssh &> /dev/null`
 fi
