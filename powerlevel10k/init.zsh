cf3e955a4b6e0c63db21975c038c27e5590565a2
Alexander van Trijffel <alexander@structura.ws>
2023-03-11 16:19:28 +0100

    feat: enable instant prompt

--- powerlevel10k/init.zsh
+++ powerlevel10k/init.zsh
@@ -1,6 +1,11 @@
 #!bin/zsh
 
 THISDIR=${0:a:h}
+
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 znap source romkatv/powerlevel10k
 
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
