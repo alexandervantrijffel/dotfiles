92feac3d1dac165abedf42060f6e44b5a196f059
Alexander van Trijffel <alexander@structura.ws>
2023-03-11 13:07:47 +0100

    feat: replace z with zoxide

--- z/init.zsh
+++ z/init.zsh
@@ -1,13 +1,10 @@
-if [[ $(lsb_release -a 2>/dev/null) =~ "Ubuntu" ]]; then 
-  znap source rupa/z z.sh
-  znap eval rupa/z "touch ${HOME}/.z"
-  # touch ${HOME}/.z
-  # zplug "rupa/z", use:"*.sh", hook-build:"touch ${HOME}/.z", defer:2
-else
-  # from packman package z
-  . /usr/share/z/z.sh
-fi
+# if [[ $(lsb_release -a 2>/dev/null) =~ "Ubuntu" ]]; then 
+#   znap source rupa/z z.sh
+#   znap eval rupa/z "touch ${HOME}/.z"
+#   # touch ${HOME}/.z
+#   # zplug "rupa/z", use:"*.sh", hook-build:"touch ${HOME}/.z", defer:2
+# else
+#   # from packman package z
+#   . /usr/share/z/z.sh
+# fi
 
-alias zgha="z alexandervantrijffel"
-alias zgh="z dev/github.com"
-alias zgl="z dev/gitlab.com"
---
+++ zoxide/init.zsh
@@ -0,0 +1,5 @@
+znap source ajeetdsouza/zoxide
+znap eval ajeetdsouza/zoxide 'zoxide init zsh --cmd z'
+alias cd="z"
+alias zgha="z alexandervantrijffel"
+alias zgh="z dev/github.com"
