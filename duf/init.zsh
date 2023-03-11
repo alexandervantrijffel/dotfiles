957b6f7165209cc0a5c2cda078d6e48001c38e82
Alexander van Trijffel <alexander@structura.ws>
2023-03-11 12:50:13 +0100

    feat: duf

--- duf/init.zsh
+++ duf/init.zsh
@@ -1,11 +1,6 @@
 if type duf 1>/dev/null; then
     alias df="duf"
+    alias dus="du | sort -h"
 fi
 
-if type ncdu 1>/dev/null; then
-    alias du="ncdu"
-else 
-    alias du="du -h --max-depth=1 "
-fi
 
-type duf 1>/dev/null && alias dus="du | sort -h"
