624d13db68a1db7465546993b5b86b0032f20a96
Alexander van Trijffel <alexander@structura.ws>
2023-03-11 12:56:24 +0100

    chore: cleanup z

--- z/init.zsh
+++ z/init.zsh
@@ -8,19 +8,6 @@ else
   . /usr/share/z/z.sh
 fi
 
-alias zfbe="z ps-737migration-be"
-alias zfbev="z ps-737migration-be && v"
-alias zfbew="z ps-737migration-be/workspace"
-
-alias zffe="z ps-licenseadmin-fe"
-alias zffev="z ps-licenseadmin-fe && v"
-alias zffey="z ps-licenseadmin-fe && yrs"
-
 alias zgha="z alexandervantrijffel"
 alias zgh="z dev/github.com"
 alias zgl="z dev/gitlab.com"
-
-alias ztsb="z trainservicebus"
-alias tsbv="z trainservicebus && v"
-
-alias iosv="z ios && v"
