469ac214d9359aae1a1257711a90cf35051a6200
Alexander van Trijffel <alexander@structura.ws>
2023-03-11 11:30:47 +0100

    fix: xmodmap for arch

--- keyboard/init.zsh
+++ keyboard/init.zsh
@@ -8,6 +8,10 @@ type setxkbmap > /dev/null 2>&1 && setxkbmap -option ctrl:nocaps
 stty start undef  # disable C-s stopping receiving keyboard signals.
 stty stop undef
 
-# if type xmodmap > /dev/null 2>&1; then
-#     xmodmap $THISDIR/.Xmodmap
-# fi
+if type xmodmap > /dev/null 2>&1; then
+  xmodmap $THISDIR/.Xmodmap
+  sysmodmap=/etc/X11/xinit/.Xmodmap
+  if [ -f $sysmodmap ]; then
+      xmodmap $sysmodmap
+  fi
+fi
--- x/init.zsh
+++ x/init.zsh
@@ -10,17 +10,17 @@ func () {
   fi
 
   if [[ $CURPREF =~ "hidpi" ]]; then
-    # if type notify-send 1>/dev/null; then
-    #   notify-send -i display "Activating hidpi"
-    # fi 
-
+    if type notify-send 1>/dev/null; then
+      notify-send -i display "Activating hidpi"
+    fi 
     GDK_SCALE=2
     GDK_DPI_SCALE=0.5
     QT_AUTO_SCREEN_SCALE_FACTOR=1
+    xrandr --output Virtual-1 --auto --mode 2560x1600 --pos 0x0 --rotate normal --dpi 227 --primary --filter nearest
   else
-    # if type notify-send 1>/dev/null; then
-    #   notify-send -i display "Activating lodpi"
-    # fi 
+    if type notify-send 1>/dev/null; then
+      notify-send -i display "Activating lodpi"
+    fi 
 
     GDK_SCALE=1
     GDK_DPI_SCALE=1.0
