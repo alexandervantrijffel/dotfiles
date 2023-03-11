5ebafc60b8b178eca4c63a52c12275fe876b5e90
Alexander van Trijffel <alexander@structura.ws>
2023-03-11 13:40:38 +0100

    feat: k

--- k8s/init.zsh
+++ k8s/init.zsh
@@ -1,6 +1,10 @@
 [ -d $HOME/.krew ] &&  PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 export KUBECONFIG=$HOME/.kube/config
-type kubectl &>/dev/null && source <(kubectl completion zsh)
 
-alias kgpw="kubectl get pods -w"
-alias kns="kubectl ns"
+type kubectl &>/dev/null && {
+  source <(kubectl completion zsh)
+
+  alias k="kubectl"
+  alias kgpw="kubectl get pods -w"
+  alias kns="kubectl ns"
+}
