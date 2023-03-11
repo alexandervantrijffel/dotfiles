c664e67903979d581233fde2bda779db057754c0
Alexander van Trijffel <alexander@structura.ws>
2023-03-11 12:49:34 +0100

    feat: different configs for alacritty depending on dpi

--- alacritty/zprofile.zsh
+++ alacritty/zprofile.zsh
@@ -1,8 +1,22 @@
-#!/usr/bin/sh
-FOLDER=$HOME/.config/alacritty
-mkdir -pv $FOLDER
+#!/usr/bin/zsh
+THISDIR=$(dirname ${0:A})
 
-CURDIR=$(dirname ${0:A})  
-[ ! -e $FOLDER/alacritty.yml ] && ln -s $CURDIR/alacritty.yml $FOLDER/alacritty.yml
+TARGETFOLDER=$HOME/.config/alacritty
+mkdir -pv $TARGETFOLDER
 
 [ ! -e /usr/share/applications/alacritty.desktop ] && sudo ln -s $CURDIR/alacritty.desktop /usr/share/applications/alacritty.desktop
+
+. ${DOTFILES}/x/ishidpi.zsh
+
+if [[ $(lsb_release -a 2>/dev/null) =~ "Arch" ]]; then 
+  if ishidpi; then
+    ln -sf $THISDIR/alacritty.arch.hidpi.yml $TARGETFOLDER/alacritty.yml
+  else 
+    echo picking lodpi
+    ln -sf $THISDIR/alacritty.arch.lodpi.yml $TARGETFOLDER/alacritty.yml
+  fi
+else
+  ln -sf $THISDIR/alacritty.yml $TARGETFOLDER/alacritty.yml
+fi
+
+
