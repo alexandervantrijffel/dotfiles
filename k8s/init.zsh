d706b36d4513cbfd6b3aa1fa69e130f76fbb46cc
Alexander van Trijffel <alexander@structura.ws>
2023-03-11 16:24:56 +0100

    feat: install kubectx and ns

--- k8s/init.zsh
+++ k8s/init.zsh
@@ -3,6 +3,7 @@ export KUBECONFIG=$HOME/.kube/config
 
 type kubectl &>/dev/null && {
   source <(kubectl completion zsh)
+  znap install ahmetb/kubectx
 
   alias k="kubectl"
   alias kgpw="kubectl get pods -w"
