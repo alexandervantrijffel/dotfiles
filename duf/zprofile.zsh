bf58b03e76cb223c8185a93ca73a7c858ab8652e
Alexander van Trijffel <alexander@structura.ws>
2023-03-11 12:50:13 +0100

    feat: duf

--- duf/zprofile.zsh
+++ duf/zprofile.zsh
@@ -1,6 +1,5 @@
 if ! type duf 1>/dev/null; then
-  ARCH=$(dpkg --print-architecture)
-  if [[ "$ARCH" != *"arm64"* ]]; then 
+  if [[ $(lsb_release -a 2>/dev/null) =~ "Ubuntu" ]]; then 
     wget https://github.com/muesli/duf/releases/download/v0.6.0/duf_0.6.0_linux_amd64.deb
     sudo dpkg -i duf_0.6.0_linux_amd64.deb
     rm -f duf_0.6.0_linux_amd64.deb
