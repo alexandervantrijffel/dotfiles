9cf1ef199b2bd91e7f2a87d45b5462f761d50ea5
Alexander van Trijffel <alexander@structura.ws>
2023-03-11 11:31:00 +0100

    chore: update scaling config

--- x/init.zsh
+++ x/init.zsh
@@ -1,31 +1,30 @@
 #!/bin/zsh
 . ${0:a:h}/togglexdpi.zsh
 
-func () {
-  local PREFFILE=$HOME/.config/X11/.Xresources-dpi-preference
-  if [ -s $PREFFILE ]; then
-    local CURPREF=$(cat $PREFFILE)
-  else
-    local CURPREF=lodpi
-  fi
+PREFFILE=$HOME/.config/X11/.Xresources-dpi-preference
+if [ -s $PREFFILE ]; then
+  CURPREF=$(cat $PREFFILE)
+else
+  CURPREF=lodpi
+fi
 
-  if [[ $CURPREF =~ "hidpi" ]]; then
-    if type notify-send 1>/dev/null; then
-      notify-send -i display "Activating hidpi"
-    fi 
-    GDK_SCALE=2
-    GDK_DPI_SCALE=0.5
-    QT_AUTO_SCREEN_SCALE_FACTOR=1
-    xrandr --output Virtual-1 --auto --mode 2560x1600 --pos 0x0 --rotate normal --dpi 227 --primary --filter nearest
-  else
-    if type notify-send 1>/dev/null; then
-      notify-send -i display "Activating lodpi"
-    fi 
+if [[ $CURPREF =~ "hidpi" ]]; then
+  if type notify-send 1>/dev/null; then
+    notify-send -i display "Activating hidpi"
+  fi 
+  export GDK_SCALE=2
+  export GDK_DPI_SCALE=0.5
+  export QT_SCALE_FACTOR=2
+  xrandr --output Virtual-1 --auto --pos 0x0 --rotate normal --dpi 227 --primary --filter nearest --primary
+else
+  if type notify-send 1>/dev/null; then
+    notify-send -i display "Activating lodpi"
+  fi 
+  export QT_SCALE_FACTOR=1
+  export GDK_SCALE=1
+  export GDK_DPI_SCALE=1.0
+  xrandr --output Virtual-1 --auto --pos 0x0 --rotate normal --dpi 110 --primary --filter nearest --primary
+fi
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+export QT_ENABLE_HIGHDPI_SCALING=1
 
-    GDK_SCALE=1
-    GDK_DPI_SCALE=1.0
-    QT_AUTO_SCREEN_SCALE_FACTOR=1
-  fi
-}
-func
-unset -f func
