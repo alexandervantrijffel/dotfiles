f74da087404141b320cf81c33cbc5dd163aa42ae
Alexander van Trijffel <alexander@structura.ws>
2023-03-16 06:29:05 +0100

    x: no xrandr on new terminal

--- x/init.zsh
+++ x/init.zsh
@@ -12,7 +12,7 @@ if ishidpi; then
   export GDK_SCALE=2
   export GDK_DPI_SCALE=0.5
   export QT_SCALE_FACTOR=2
-  xrandr --output Virtual-1 --auto --pos 0x0 --rotate normal --dpi 192 --primary --filter nearest
+  # xrandr --output Virtual-1 --auto --pos 0x0 --rotate normal --dpi 192 --primary --filter nearest
 else
   if type notify-send 1>/dev/null; then
     notify-send -i display "lodpi mode" -u low
@@ -20,7 +20,7 @@ else
   export QT_SCALE_FACTOR=1
   export GDK_SCALE=1
   export GDK_DPI_SCALE=1.0
-  xrandr --output Virtual-1 --auto --pos 0x0 --rotate normal --dpi 110 --primary --filter nearest --primary
+  # xrandr --output Virtual-1 --auto --pos 0x0 --rotate normal --dpi 110 --primary --filter nearest --primary
 fi
 
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
