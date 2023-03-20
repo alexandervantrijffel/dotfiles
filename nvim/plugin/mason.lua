5101286bc17205f9b8b71ac9596ad4a139b7d71d
Alexander van Trijffel <alexander@structura.ws>
2023-03-20 22:41:58 +0100

    nvim: add additional mason installs

--- nvim/plugin/mason.lua
+++ nvim/plugin/mason.lua
@@ -8,6 +8,9 @@ local nvim_lsp = require('lspconfig')
 nvim_lsp.bashls.setup{}
 nvim_lsp.docker_compose_language_service.setup{}
 nvim_lsp.dockerls.setup{}
+nvim_lsp.dockerls.setup{}
+nvim_lsp.golangci_lint_ls.setup{}
 nvim_lsp.gopls.setup{}
 nvim_lsp.tailwindcss.setup{}
+nvim_lsp.tsserver.setup{}
 nvim_lsp.yamlls.setup{}
