acd7ca62259b6a3317a6f7ab964a0adaf18bb9f6
Alexander van Trijffel <alexander@structura.ws>
2023-03-24 21:10:07 +0100

    feat: export env vars based on lo/hidpi

--- x/zprofile.zsh
+++ x/zprofile.zsh
@@ -14,3 +14,14 @@ fi
 [ ! -L $XDG_CONFIG_HOME/X11/.xinitrc ] && {
  ln -s $THISDIR/.xinitrc $XDG_CONFIG_HOME/X11/.xinitrc
 }
+
+. $THISDIR/functions/ishidpi
+if ishidpi; then
+  export GDK_SCALE=2
+  export GDK_DPI_SCALE=0.5
+  export QT_SCALE_FACTOR=2
+else
+  export GDK_SCALE=1
+  export GDK_DPI_SCALE=1
+  export QT_SCALE_FACTOR=1
+fi
