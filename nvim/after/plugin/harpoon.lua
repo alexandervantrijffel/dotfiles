1178f33d378c03db76566385558d538abc9ef1b6
Alexander van Trijffel <alexander@structura.ws>
2023-04-02 11:10:43 +0200

    nvim: harpoon keymaps

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -3,3 +3,8 @@ local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "ha", mark.add_file)
 vim.keymap.set("n", "hm", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "hz", function() ui.nav_file(1) end)
+vim.keymap.set("n", "hx", function() ui.nav_file(2) end)
+vim.keymap.set("n", "hc", function() ui.nav_file(3) end)
+vim.keymap.set("n", "hv", function() ui.nav_file(4) end)
