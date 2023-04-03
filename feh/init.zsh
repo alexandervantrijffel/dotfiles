9cd274d3a86d462e08042948f919f9100ec7df89
Alexander van Trijffel <alexander@structura.ws>
2023-04-03 06:16:01 +0200

    feh: formatting

--- feh/init.zsh
+++ feh/init.zsh
@@ -1,13 +1,13 @@
 if type feh 1>/dev/null; then
-  BGFOLDER=$HOME/Pictures/backgrounds
-  [[ ! -d $BGFOLDER ]] && BGFOLDER=$HOME/pictures/backgrounds
+    BGFOLDER=$HOME/Pictures/backgrounds
+    [[ ! -d $BGFOLDER ]] && BGFOLDER=$HOME/pictures/backgrounds
 
-  [ -d $BGFOLDER ] && feh --slideshow-delay 1800 --bg-fill $BGFOLDER -z -r -D1800
+    [ -d $BGFOLDER ] && feh --slideshow-delay 1800 --bg-fill $BGFOLDER -z -r -D1800
 
-  # --bg-tile
-  # --bg-fill or --bg-scale?
-  # or use --zoom fill
-  
-  # foreground window
-  alias fehfg='nohup feh -zrs --scale-down --image-bg black --slideshow-delay 3600 ~/Pictures/fehbackgrounds > /tmp/feh.nohup.out.${RANDOM} &'
+    # --bg-tile
+    # --bg-fill or --bg-scale?
+    # or use --zoom fill
+
+    # foreground window
+    alias fehfg='nohup feh -zrs --scale-down --image-bg black --slideshow-delay 3600 ~/Pictures/fehbackgrounds > /tmp/feh.nohup.out.${RANDOM} &'
 fi
