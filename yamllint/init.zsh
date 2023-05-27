a111012737d2120ef1aeba38eefcae1b43e4766e
Alexander van Trijffel <alexander@structura.ws>
2023-05-27 09:46:04 +0200

    yamllint: remove unneeded config

--- yamllint/init.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-THISDIR=${0:a:h}
-
-if [ ! -L $XDG_CONFIG_HOME/yamllint/config ] ; then
-    mkdir -p $XDG_CONFIG_HOME/yamllint
-    ln -s $THISDIR/config $XDG_CONFIG_HOME/yamllint/config 
-fi
