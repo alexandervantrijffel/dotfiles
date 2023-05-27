6fb738864150f93ba0148f002f906b7476ddd4ab
Alexander van Trijffel <alexander@structura.ws>
2023-05-27 10:28:20 +0200

    chore: cleanup updateall

--- sh/updateall.zsh
+++ sh/updateall.zsh
@@ -4,29 +4,28 @@ THISDIR=${0:a:h}
 tldr -u
 
 go clean -modcache
-go get -u all
 
 echo updating yarn packages
-yarn global upgrade
+# yarn global upgrade
 yarn cache clean --all
 
 npm cache clean --force
 
 echo updating python packages
 sudo python3 -m pip install --upgrade pip
-pip install --upgrade pip setuptools wheel
-pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo -H python3 -m pip install -U
+# pip install --upgrade pip setuptools wheel
+# pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo -H python3 -m pip install -U
 pip cache purge
 
-echo apt update and upgrade
-sudo apt update
-sudo apt upgrade -y
+# echo apt update and upgrade
+# sudo apt update
+# sudo apt upgrade -y
 
-echo snap refresh and clear cache
-sudo snap refresh
-sudo rm /var/lib/snapd/cache/*
-# 2 is the minimum
-sudo snap set system refresh.retain=2
+# echo snap refresh and clear cache
+# sudo snap refresh
+# sudo rm /var/lib/snapd/cache/*
+# # 2 is the minimum
+# sudo snap set system refresh.retain=2
 
 echo updating git repositories
 { cd /opt/fromgit && find . -maxdepth 1 -type d -exec git --git-dir={}/.git --work-tree=$(pwd)/{} pull origin master \; }
@@ -40,17 +39,18 @@ if [[ "$ARCH" =~ "arm64" ]]; then
     sudo mv nvim.appimage ${DIR}/nvim
 fi
 
-{ cd ~/.SpaceVim && git pull }
+nvim --noplugin +PlugUpgrade +PlugUpdate +PlugClean! +qall
+
+# { cd ~/.SpaceVim && git pull }
 
 if [[ "$ARCH" =~ "arm64" ]]; then
     { cd /usr/local/bin && sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" }
 else
     { cd /usr/local/bin && sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl" }
-}
 fi
 sudo chmod +x /usr/local/bin/kubectl
 
-docker system prune -a -f
+sudo docker system prune -a -f
 
 type notify-send 1>/dev/null && notify-send -i display "updateall.sh completed"
 
@@ -68,5 +68,3 @@ nvm upgrade
 pacman -Syu
 
 rustup update
-
-nvim --noplugin +PlugUpgrade +PlugUpdate +PlugClean! +qall
