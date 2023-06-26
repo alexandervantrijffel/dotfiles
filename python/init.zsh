dbad00058f017852fadf29172e24c0031e925472
Alexander van Trijffel <alexander@structura.ws>
2023-06-26 11:39:05 +0200

    python: improve pyenv missing handling

--- python/init.zsh
+++ python/init.zsh
@@ -1,4 +1,9 @@
-eval "$(pyenv init -)"
+if type pyenv 1>/dev/null; then
+  eval "$(pyenv init -)"
+else 
+  echo "pyenv not found"
+fi
+
 export WORKON_HOME=$HOME/.config/.virtualenvs
 # source /usr/local/bin/virtualenvwrapper.sh
 export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
