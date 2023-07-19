2ae7351ce7c4cc133c95a5dde638e7de91037015
Alexander van Trijffel <alexander@structura.ws>
2023-07-19 21:39:12 +0200

    python: improve pyenv setup

--- python/init.zsh
+++ python/init.zsh
@@ -1,4 +1,6 @@
 if type pyenv 1>/dev/null; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
   eval "$(pyenv init -)"
 else 
   echo "pyenv not found"
