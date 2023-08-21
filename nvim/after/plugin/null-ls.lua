8fd123b41f3c4e7853e6cf639dec0e9b67910a7c
Alexander van Trijffel <alexander@structura.ws>
2023-08-21 17:00:27 +0200

    nvim: disable prettierd for yaml because of merging comment lines with previous line

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -5,20 +5,15 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
   -- find more sources at https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
   sources = {
-
     null_ls.builtins.diagnostics.eslint_d.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
+    null_ls.builtins.formatting.eslint_d,
     null_ls.builtins.diagnostics.jsonlint,
     null_ls.builtins.formatting.fixjson,
 
-    -- null_ls.builtins.formatting.eslint_d
-    null_ls.builtins.formatting.prettierd,
-    -- don't enable this; it leads to performance issues and issues with undo
-    -- null_ls.builtins.diagnostics.tsc,
-
-    -- null_ls.builtins.code_actions.ltrs,     -- rust
-    -- null_ls.builtins.diagnostics.ltrs,
+    null_ls.builtins.code_actions.ltrs,     -- rust
+    null_ls.builtins.diagnostics.ltrs,
     null_ls.builtins.formatting.rustfmt,
 
     null_ls.builtins.code_actions.gitsigns,
@@ -28,21 +23,28 @@ null_ls.setup({
 
     null_ls.builtins.formatting.golines,
     null_ls.builtins.code_actions.gomodifytags,
-    -- don't use https://github.com/golangci/golangci-lint/issues/2654 is solved
-    -- this is replaced with a custom config for golangci-lint in lspconfig
-    -- null_ls.builtins.diagnostics.golangci_lint,
-
     null_ls.builtins.diagnostics.luacheck,
 
     null_ls.builtins.diagnostics.markdownlint,
     -- null_ls.builtins.formatting.remark,
 
-    -- null_ls.builtins.diagnostics.markuplint,     -- html
-    null_ls.builtins.diagnostics.yamllint,
+    -- null_ls.builtins.diagnostics.tsc,
+    -- null_ls.builtins.diagnostics.yamllint,
     null_ls.builtins.diagnostics.zsh,
     null_ls.builtins.formatting.beautysh,
 
     null_ls.builtins.formatting.rustywind,     -- tailwindcss
+
+    null_ls.builtins.formatting.prettierd.with({
+      filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "css", "scss", "less", "html", "json", "jsonc",  "markdown", "markdown.mdx", "graphql", "handlebars" }
+      -- this is disabled because it merges # comments in yaml with the previous line which sucks
+      -- "yaml",
+    })
+    -- null_ls.builtins.diagnostics.markuplint,     -- html
+    -- don't use https://github.com/golangci/golangci-lint/issues/2654 is solved
+    -- this is replaced with a custom config for golangci-lint in lspconfig
+    -- null_ls.builtins.diagnostics.golangci_lint,
+    -- don't enable this; it leads to performance issues and issues with undo
   },
   -- format on save
   on_attach = function(client, bufnr)
