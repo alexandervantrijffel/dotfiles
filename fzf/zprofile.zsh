1b127b006713dee6b65230a718bfe921d1d95a1f
Alexander van Trijffel <alexander@structura.ws>
2023-09-04 10:29:39 +0200

    atuin: disable fzf key bindings to apply atuin for ctrl+r

--- fzf/zprofile.zsh
+++ fzf/zprofile.zsh
@@ -2,7 +2,8 @@ rm -f $HOME/.local/bin/uninstall
 rm -f $HOME/.local/bin/install
 
 ! type fzf 1>/dev/null && {
-    znap source junegunn/fzf shell/{completion,key-bindings}.zsh
+    # znap source junegunn/fzf shell/{completion,key-bindings}.zsh
+    znap source junegunn/fzf shell/{completion}.zsh
     /opt/fromgit/fzf/install --no-fish --no-bash --bin
     znap install junegunn/fzf
     rm -f $HOME/.local/bin/install $HOME/.local/bin/uninstall
