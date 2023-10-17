6574cb72bb4aa8084980db85a7cd8a6eb22b2ebd
Alexander van Trijffel <alexander@structura.ws>
2023-10-17 07:16:06 +0200

    rust: no default RUST_LOG

--- rust/init.zsh
+++ rust/init.zsh
@@ -2,7 +2,7 @@
 THISDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 export RUSTC_WRAPPER=sccache
-export RUST_LOG=debug
+# export RUST_LOG=debug
 
 FPATH=${FPATH}:$THISDIR/functions
 autoload -z abr
