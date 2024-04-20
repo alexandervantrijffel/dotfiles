ed8bfd8be3b2d0326501cb00093d0220b349afec
Alexander van Trijffel <alexander@structura.ws>
2024-04-20 08:02:50 +0200

    pacman: update installed packages

--- python/init.zsh
+++ python/init.zsh
@@ -2,8 +2,6 @@ if type pyenv 1>/dev/null; then
   export PYENV_ROOT="$HOME/.pyenv"
   command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
   eval "$(pyenv init -)"
-else 
-  echo "pyenv not found"
 fi
 
 export WORKON_HOME=$HOME/.config/.virtualenvs
