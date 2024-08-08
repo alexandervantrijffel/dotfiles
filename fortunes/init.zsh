e7f080135a16de6abc873bf2072e6670d809f45c
Alexander van Trijffel <alexander@structura.ws>
2024-08-08 22:06:51 +0200

    pacman: update installed packages

--- fortunes/init.zsh
+++ fortunes/init.zsh
@@ -1,5 +1,4 @@
 type fortune 1>/dev/null && {
-
     fortune | xargs -0 echo -e "     $(date "+ %A %e %B %R Week %V")\n\n"
     # fortune | cowsay -f meow | xargs -0 echo -e "     $(date "+ %A %e %B %R Week %V")\n"
 }
