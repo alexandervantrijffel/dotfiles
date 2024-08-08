7e534dc5b18fd7e6820ce2cc9c19a4f953084243
Alexander van Trijffel <alexander@structura.ws>
2024-08-09 00:41:31 +0200

    archive various unused tools

--- qutebrowser/init.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-THISDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
-if [[ ! -L $XDG_CONFIG_HOME/qutebrowser/autoconfig.yml ]];then
-  mkdir -p $XDG_CONFIG_HOME/qutebrowser
-  ln -s $THISDIR/autoconfig.yml $XDG_CONFIG_HOME/qutebrowser/
-fi
