995d9e0aeef93cfd43b2b261e887b48260ad0858
Alexander van Trijffel <alexander@structura.ws>
2024-09-19 13:27:25 +0200

    rust: remove unused functions

--- rust/init.zsh
+++ rust/init.zsh
@@ -2,7 +2,7 @@
 THISDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 export RUSTFLAGS="-L /usr/lib/aarch64-linux-gnu"
-export RUSTC_WRAPPER=sccache
+# export RUSTC_WRAPPER=sccache
 # export RUST_LOG=debug
 #
 export CARGO_TARGET_DIR=/tmp/target
@@ -11,13 +11,10 @@ export MYSQLCLIENT_LIB_DIR=/usr/lib/aarch64-linux-gnu/
 export MYSQLCLIENT_VERSION=8.0.37
 
 FPATH=${FPATH}:$THISDIR/functions
-autoload -z abr
-autoload -z samabr
 
 FPATH=${FPATH}:$THISDIR/functions
 autoload -z ago
 autoload -z ato
-autoload -z atdr
 
 [ -d ~/.cargo/bin ] && export PATH="$PATH:$HOME/.cargo/bin"
 
