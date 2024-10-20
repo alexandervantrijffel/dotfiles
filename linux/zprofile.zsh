4fa31518737029bd51ab0542a6362f40d7305d0a
Alexander van Trijffel <alexander@structura.ws>
2024-10-20 08:25:26 +0200

    ulimits: don't set for now

--- linux/zprofile.zsh
+++ linux/zprofile.zsh
@@ -3,7 +3,7 @@ THISDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 [ ! -L /etc/security/limits.conf ] && {
   sudo rm -f /etc/security/limits.conf
-  sudo ln -s ${THISDIR}/limits.conf /etc/security/limits.conf
+  # sudo ln -s ${THISDIR}/limits.conf /etc/security/limits.conf
   # also make sure to add `session required pam_limits.so` to `/etc/pam.d/*` files
   #
   # check and set system wide file descriptor limit:
@@ -13,7 +13,7 @@ THISDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 }
 
 [ -d /etc/systemd ] && [ ! -L /etc/systemd/system.conf.d/99-filelimit.conf ] && {
-  sudo mkdir -p /etc/systemd/system.conf.d
-  sudo rm -f /etc/systemd/system.conf.d/99-filelimit.conf
-  sudo ln -s ${THISDIR}/99-services-filelimit.conf /etc/systemd/system.conf.d/
+  # sudo mkdir -p /etc/systemd/system.conf.d
+  # sudo rm -f /etc/systemd/system.conf.d/99-filelimit.conf
+  # sudo ln -s ${THISDIR}/99-services-filelimit.conf /etc/systemd/system.conf.d/
 }
